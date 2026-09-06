bc96e20b5688a4e3c386f73ec6b8210fe6817f76
revolc-hiuga <karotte.monoclock@gmail.com>
2026-09-06 19:14:23 +0800

    20260906 1913 # 建立新项目

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("欢迎来到祢水象棋。")
+    print_version()
+
+def print_version():
+    print("祢水象棋 V1.0 build: 1")
+    print("作者：Xack & 日向")
+    print()
+
+if __name__ == "__main__":
+    main()
