45899429d0c319a2076ff80bc025a096c5347452
revolc-hiuga <karotte.monoclock@gmail.com>
2026-09-08 19:52:37 +0800

    20260908 1952 # 加入电脑难度选择

--- src/main.py
+++ src/main.py
@@ -5,18 +5,21 @@ def main():
     print_version()
 
     mode = select_mode()
-    print()
 
-    print("mode = ", mode)
+    com_depth = 0
+    if mode != const.TWO_PLAYERS:
+        com_depth = select_com_depth()
+
+    print("mode =", mode, ", com_depth =", com_depth)
+
 
 def print_version():
-    print("祢水象棋 V1.0 build: 2")
+    print("祢水象棋 V1.0 build: 3")
     print("作者：Xack & 日向")
     print()
 
-# 难办的问题。如果一定要在这个函数下满足末尾输出换行的话，
-# 就要多次插入换行语句，不美观。
 def select_mode() -> int:
+    print("【模式选择】")
     print("1. 双人模式")
     print("2. 单人模式，人类先行")
     print("3. 单人模式，电脑先行")
@@ -24,22 +27,46 @@ def select_mode() -> int:
     s = input("请选择你想要的模式（1~3, 默认为1）：").strip()
     if not s.isdigit():
         if s == "":
-            print("没有输入。默认选择为双人模式。")
+            print("没有输入。默认选择双人模式。\n")
         else:
-            print("格式错误。默认选择为双人模式。")
+            print("格式错误。默认选择双人模式。\n")
         return const.TWO_PLAYERS
 
     mode = int(s)
-    if mode == 1:
-        return const.TWO_PLAYERS
-    elif mode == 2:
-        return const.ONE_PLAYER_HUMAN_FIRST
-    elif mode == 3:
-        return const.ONE_PLAYER_COMPUTER_FIRST
-    else:
-        print("格式错误。默认选择为双人模式。")
-        return const.TWO_PLAYERS
+    if not (mode == const.TWO_PLAYERS 
+         or mode == const.ONE_PLAYER_HUMAN_FIRST 
+         or mode == const.ONE_PLAYER_COMPUTER_FIRST):
+        print("格式错误。默认选择双人模式。")
+        mode = const.TWO_PLAYERS
+
+    print()
+    return mode
         
+
+def select_com_depth() -> int:
+    depth_table = [3, 5, 7]
+
+    print("【难度选择】")
+    print("1. 简易 (depth=3)")
+    print("2. 中等 (depth=5)")
+    print("3. 困难 (depth=7)")
+
+    print("难度越大，代表电脑走棋越智能，也代表电脑思考的时间越长。")
+    s = input("请输入你想要的难度（1~3, 默认为1)：").strip()
+    if not s.isdigit():
+        if s == "":
+            print("没有输入。默认选择简易模式。\n")
+        else:
+            print("格式错误。默认选择简易模式。\n")
+        return depth_table[0]
+
+    index = int(s)
+    if index < 1 or index > 3:
+        print("格式错误。默认选择简易模式。\n")
+        return depth_table[0]
+    else:
+        print()
+        return depth_table[index-1]
 
 if __name__ == "__main__":
     main()
